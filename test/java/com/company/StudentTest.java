c1ffbdc1c2e78b5a3079c234c0049408710a13ed
kafriyiegyamfi <kafriyiegyamfi@gmail.com>
2020-10-09 12:18:43 +0000

    added Testfile

---
+++ test/java/com/company/StudentTest.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import org.junit.Before;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+    @Before
+
+
+    @org.junit.jupiter.api.Test
+    void getAvg_grade() {
+
+    }
+}

0ed97903f8eebf1edab93665733b101ee1d256b0
kafriyiegyamfi <kafriyiegyamfi@gmail.com>
2020-10-09 13:57:32 +0000

    Testing Student

--- test/java/com/company/StudentTest.java
+++ test/java/com/company/StudentTest.java
@@ -1,15 +1,27 @@
 package com.company;
 
-import org.junit.Before;
+
+
+import java.util.ArrayList;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentTest {
-    @Before
+
+
+
+
+
 
 
     @org.junit.jupiter.api.Test
     void getAvg_grade() {
-
+        ArrayList<Integer> st_grade = new ArrayList<>();
+        st_grade.add(20);
+        st_grade.add(20);
+        st_grade.add(20);
+        Student student= new Student("Emma","1234A", st_grade, "100" );
+        assertTrue((20.6+20.6+20.6)/3 == student.getAvg_grade());
     }
 }
